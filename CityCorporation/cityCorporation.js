65c6d9d36015810942a769ed9494015e03d248a3
Rifaatislaam <mdrifaatislaam@gmail.com>
2026-04-07 14:53:38 +0600

    clear the file

--- CityCorporation/cityCorporation.js
+++ CityCorporation/cityCorporation.js
@@ -1,33 +1,37 @@
-const swiper = new Swiper(".slider-wrapper", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 20,
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
 
+const images = document.querySelectorAll(".image");
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+let slideNumber = 1;
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const length = images.length;
 
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    620: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 4,
-    }
-  },
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+};
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+  slideNumber--;
+};
+
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+  slideNumber = length;
+};
+
+right.addEventListener("click", () => {
+  slideNumber < length ? nextSlide() : getFirstSlide();
 });
 
-
+left.addEventListener("click", () => {
+  slideNumber > 1 ? prevSlide() : getLastSlide();
+});
